21df2aae5a2dc1def790b3ffc82cc14266cfc60b
Nicolas Ibarra <niicoibarra1@gmail.com>
2022-12-04 15:34:30 -0300

    Restructuring Users resolver

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -9,39 +9,49 @@ module.exports = {
     // Parent param refers to the others resolvers results, in case we have it.
     // The second param refers to the registerInput param from the typeDefs call.
     // Info param refers to some metadata.
-    async registerUser(parent, { registerInput }, context, info) {
+    async RegisterUser(parent, { registerInput }, context, info) {
       let { userName, password, confirmPassword, email } = registerInput;
 
       // Hashing password. We need bcryptjs package.
-      password = await BCRYPT.hash(password, 12);
-      const NEWUSER = new USERSMODEL({
-        userName,
-        email,
-        password,
-        createdAt: new Date().toISOString(),
-      });
-      const RES = await NEWUSER.save();
-
+      password = await HashPassword(password);
+      // Instantiate a new user.
+      const USER = InstantiateNewUser(registerInput);
+      // Saving the new user in the database.
+      const RESPONSE = await USER.save();
       // Creating auth token. We need jsonwebtoken package.
-      const token = JWT.sign(
-        {
-          id: RES.id,
-          userName: RES.userName,
-          email: RES.email,
-        },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
+      const token = CreateToken(RESPONSE);
 
       return {
-        ...RES._doc,
-        id: RES._id,
+        ...RESPONSE._doc,
+        id: RESPONSE._id,
         token,
       };
     },
   },
 };
 
-// function HashPassword(userName, password, confirmPassword, email){
+const HashPassword = async password => await BCRYPT.hash(password, 12);
 
-// }
+const InstantiateNewUser = registerInput => {
+  let { userName, password, email } = registerInput;
+  const NEWUSER = new USERSMODEL({
+    userName,
+    email,
+    password,
+    createdAt: new Date().toISOString(),
+  });
+  return NEWUSER;
+}
+
+const CreateToken = RESPONSE => {
+  const token = JWT.sign(
+    {
+      id: RESPONSE.id,
+      userName: RESPONSE.userName,
+      email: RESPONSE.email,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+  return token;
+}
